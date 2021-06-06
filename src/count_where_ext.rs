809ec647d4152a4ad124e720e1a2194b61e9a6a2
Bryce Campbell <tonyhawk2100@gmail.com>
2021-06-06 10:36:24 -0700

    continue working on library.

--- src/count_where_ext.rs
+++ src/count_where_ext.rs
@@ -10,7 +10,7 @@ impl<I> CountWhereExt for I where I: Iterator {
      * count elements matching given predicate.
      * # Example
      * let numbers = [5, 5, 5, 2, 1];
-     * assert_eq1(3, numbers.iter().count_where(|n| **n == 5)); */ 
+     * assert_eq!(3, numbers.iter().count_where(|n| **n == 5)); */ 
 	fn count_where<P: FnMut(&Self::Item) -> bool>(self, predicate: P)-> usize {
 		
         // count up items found in filter.
